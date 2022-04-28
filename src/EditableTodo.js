5a0b24664785a9f4ed0fb6e20d63a740ef57c06b
Yuri <yuri.belorusets@gmail.com>
2022-04-28 16:01:49 -0700

    did Todo component and Editable Todo

---
+++ src/EditableTodo.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import Todo from "./Todo";
+import TodoForm from "./TodoForm";
+
+/** Show editable todo item.
+ *
+ * Props
+ * - todo
+ * - update(): fn to call to update a todo
+ * - remove(): fn to call to remove a todo
+ *
+ * EditableTodoList -> EditableTodo -> { Todo, TodoForm }
+ */
+
+function EditableTodo({todo, update, remove}) {
+
+  let editing = false;
+
+  /** Toggle if this is being edited */
+  function toggleEdit() { 
+    editing = !editing;
+  }
+
+  /** Call remove fn passed to this. */
+  function handleDelete() {
+    remove(todo.id);
+   }
+
+  /** Edit form saved; toggle isEditing and update in ancestor. */
+  function handleSave(formData) {
+    update({...formData});
+    toggleEdit();
+   }
+
+  return (
+      <div className="EditableTodo">
+
+                {editing ? 
+
+                <TodoForm handleSave={handleSave} initialFormData={todo}/>
+
+                :
+
+                <div className="mb-3">
+                  <div className="float-end text-sm-end">
+                    <button
+                        className="EditableTodo-toggle btn-link btn btn-sm"
+                        onClick={toggleEdit}>
+                      Edit
+                    </button>
+                    <button
+                        className="EditableTodo-delBtn btn-link btn btn-sm text-danger"
+                        onClick={handleDelete}>
+                      Del
+                    </button>
+                  </div>
+                  <Todo id={todo.id} title={todo.title}
+                   description={todo.description} priority={todo.priority}/>
+                </div>
+                }
+
+      </div>
+  );
+}
+
+export default EditableTodo;
---
+++ src/Todo.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+/** Simple presentation component for a todo.
+ *
+ * Props:
+ * - todo: like { id, title, description, priority }
+ *
+ * { EditableTodo, TopTodo } -> Todo
+ **/
+
+function Todo({ id, title, description, priority }) {
+  return (
+      <div id={id} className="Todo">
+        <div><b>{title}</b> <small>(priority: {priority})</small></div>
+        <div><small>{description}</small></div>
+      </div>
+  );
+}
+
+export default Todo;
